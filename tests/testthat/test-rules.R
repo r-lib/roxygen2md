b81adb9428677c9adee8ff064c75a549d91e0362
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-20 17:28:06 +0200

    - Tests make sure that transform works also when applying all rules.

--- tests/testthat/test-rules.R
+++ tests/testthat/test-rules.R
@@ -1,51 +1,56 @@
+expect_convert <- function(transform, input, output) {
+  expect_equal(transform(input), output)
+  expect_equal(markdownify(input), output)
+}
+
 test_that("convert_local_links", {
-  expect_equal(convert_local_links("\\code{\\link{b}}"), "[b()]")
-  expect_equal(convert_local_links("\\code{\\link{b}()}"), "[b()]")
+  expect_convert(convert_local_links, "\\code{\\link{b}}", "[b()]")
+  expect_convert(convert_local_links, "\\code{\\link{b}()}", "[b()]")
 })
 
 test_that("convert_special_alien_links", {
-  expect_equal(convert_special_alien_links("\\code{\\link[a:b]{a::b()}}"), "[a::b()]")
-  expect_equal(convert_special_alien_links("\\code{\\link[a:b]{a::b}()}"), "[a::b()]")
-  expect_equal(convert_special_alien_links("\\code{\\link[a:b]{a::b}}"), "[a::b()]")
+  expect_convert(convert_special_alien_links, "\\code{\\link[a:b]{a::b()}}", "[a::b()]")
+  expect_convert(convert_special_alien_links, "\\code{\\link[a:b]{a::b}()}", "[a::b()]")
+  expect_convert(convert_special_alien_links, "\\code{\\link[a:b]{a::b}}", "[a::b()]")
 })
 
 test_that("convert_alien_links", {
-  expect_equal(convert_alien_links("\\code{\\link[a]{b}}"), "[a::b()]")
-  expect_equal(convert_alien_links("\\code{\\link[a]{b}()}"), "[a::b()]")
+  expect_convert(convert_alien_links, "\\code{\\link[a]{b}}", "[a::b()]")
+  expect_convert(convert_alien_links, "\\code{\\link[a]{b}()}", "[a::b()]")
 })
 
 test_that("convert_S4_code_links", {
-  expect_equal(convert_S4_code_links("\\code{\\linkS4class{b}}"), "[b-class]")
+  expect_convert(convert_S4_code_links, "\\code{\\linkS4class{b}}", "[b-class]")
 })
 
 test_that("convert_S4_links", {
-  expect_equal(convert_S4_links("\\linkS4class{b}"), "[b-class]")
+  expect_convert(convert_S4_links, "\\linkS4class{b}", "[b-class]")
 })
 
 test_that("convert_non_code_links", {
-  expect_equal(convert_non_code_links("\\link{b}"), "[b]")
+  expect_convert(convert_non_code_links, "\\link{b}", "[b]")
 })
 
 test_that("convert_non_code_special_alien_links", {
-  expect_equal(convert_non_code_special_alien_links("\\link[a:b]{a::b}"), "[a::b]")
+  expect_convert(convert_non_code_special_alien_links, "\\link[a:b]{a::b}", "[a::b]")
 })
 
 test_that("convert_non_code_alien_links", {
-  expect_equal(convert_non_code_alien_links("\\link[a]{b}"), "[a::b]")
+  expect_convert(convert_non_code_alien_links, "\\link[a]{b}", "[a::b]")
 })
 
 test_that("convert_code", {
-  expect_equal(convert_code("\\code{x}"), "`x`")
+  expect_convert(convert_code, "\\code{x}", "`x`")
 })
 
 test_that("convert_emph", {
-  expect_equal(convert_emph("\\emph{x}"), "*x*")
+  expect_convert(convert_emph, "\\emph{x}", "*x*")
 })
 
 test_that("convert_bold", {
-  expect_equal(convert_bold("\\bold{x}"), "**x**")
+  expect_convert(convert_bold, "\\bold{x}", "**x**")
 })
 
 test_that("convert_url", {
-  expect_equal(convert_url("\\url{x}"), "<x>")
+  expect_convert(convert_url, "\\url{x}", "<x>")
 })
