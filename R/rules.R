10abcee53e2a280ec2ae470e8a5b6084a22c6af3
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-20 10:34:13 +0200

    Move code

--- R/rules.R
+++ R/rules.R
@@ -1,3 +1,48 @@
+#' Convert a character vector from Rd to Markdown
+#'
+#' @param text A character vector containing `.Rd` style annotations.
+#'
+#' @return The same vector with `.Rd` style annotations converted to Markdown
+#'   style annotations.
+#' @export
+#'
+#' @examples
+#'
+#' text <- c(
+#'   "Both \\emph{italics} and \\bold{bold} text.",
+#'   "And here we have \\emph{italics} in some text.",
+#'   "This is \\bold{bold} text."
+#' )
+#'
+#' markdownify(text)
+#'
+markdownify <- function(text) {
+  transformers <- c(
+    convert_local_links,
+    convert_special_alien_links,
+    convert_alien_links,
+    convert_S4_code_links,
+    convert_S4_code_links,
+    convert_S4_links,
+    convert_code,
+    convert_emph,
+    convert_bold,
+    convert_bold,
+    convert_url,
+    NULL
+  )
+
+  transform_element <- function(elem) {
+    Reduce(
+      function(txt, transformer) transformer(txt),
+      transformers,
+      init = elem
+    )
+  }
+
+  vapply(text, transform_element, character(1), USE.NAMES = FALSE)
+}
+
 convert_local_links <- function(text) {
   rex::re_substitutes(
     global = TRUE,
