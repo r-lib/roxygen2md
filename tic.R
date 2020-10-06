09b5f877f153b187989eeeb7a23a8e9636d4227c
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-06 05:47:36 +0200

    Move to r-lib/actions

--- tic.R
+++
@@ -1,7 +0,0 @@
-# installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks()
-
-if (ci_on_ghactions() && Sys.info()[["sysname"]] == "Linux") {
-  # creates pkgdown site and pushes to gh-pages branch
-  do_pkgdown()
-}
